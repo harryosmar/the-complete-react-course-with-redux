5359294d440e416bb22b08cc0fcb7d9ba9141f64
harryosmar <harryosmarsitohang@gmail.com>
2018-12-11 22:16:59 +0100

    JSX Expression : use variables reference is JSX

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,18 @@
 console.log('app is running');
 
-var element = <p>Hello JSX</p>;
+var user = {
+    'name' : 'Mokmok',
+    'age' : 100,
+    'location' : 'earth'
+};
+
+var element = (
+    <div>
+        <h1>{user.name.toUpperCase()}</h1>
+        <p>{user.age} years old</p>
+        <p>live at {user.location}</p>
+    </div>
+);
 
 var placeholder = document.getElementById('placedhere');
 
