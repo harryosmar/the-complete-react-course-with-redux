1cb2a3d8af748ed548d41ea994a115a054269ded
harryosmar <harryosmarsitohang@gmail.com>
2018-12-16 21:24:40 +0100

    set error message in react component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -15,6 +15,12 @@ class IndecisionApp extends React.Component {
     }
 
     addOption(newOption) {
+        if (!newOption) {
+            return 'enter valid value to add option';
+        } else if(this.state.options.indexOf(newOption) > -1) {
+            return 'option already exist';
+        }
+
         this.setState((prevState) => {
             return {
                 options: [...prevState.options, newOption]
@@ -92,23 +98,33 @@ class AddOption extends React.Component {
     constructor(props) {
         super(props);
         this.addOption = this.addOption.bind(this);
+        this.state = {
+            error: ''
+        }
     }
 
     addOption(e) {
         e.preventDefault();
         const newOption = e.target.elements.option.value.trim();;
-        if (newOption) {
-            this.props.addOption(newOption);
+        const error = this.props.addOption(newOption);
+        this.setState(() => {
+            return {error};
+        });
+
+        if (!error) {
             e.target.elements.option.value = '';
         }
     }
 
     render() {
         return (
-            <form onSubmit={this.addOption}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
+            <div>
+                {this.state.error && <p style={{color:'red'}}>{this.state.error}</p>}
+                <form onSubmit={this.addOption}>
+                    <input type="text" name="option"/>
+                    <button>Add Option</button>
+                </form>
+            </div>
         );
     }
 }
