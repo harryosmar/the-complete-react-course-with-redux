621922304e0d19c0e548abd878a94620aa9c8206
harryosmar <harryosmarsitohang@gmail.com>
2018-12-23 17:53:44 +0100

    query string and url parameters

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => (
     <div>
-        This is edit expense page
+        edit expense with id : {props.match.params.id}
     </div>
 );
 
--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink activeClassName="is-active" exact={true} to="/">Dashboard</NavLink>
         <NavLink activeClassName="is-active" to="/create">Create</NavLink>
-        <NavLink activeClassName="is-active" to="/edit">Edit</NavLink>
         <NavLink activeClassName="is-active" to="/help">Help</NavLink>
     </header>
 );
