24194811ac3773092fc09d8d046e3885850fb36b
harryosmar <harryosmarsitohang@gmail.com>
2019-01-01 20:22:09 +0100

    expensify-app: use selector to display filtered expoenses

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem.js';
+import { getVisibleExpenses } from '../selectors/expenses.js';
 
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List :</h1>
-        { props.expenses.map((expense, index) => <ExpenseListItem { ...expense } key={index}/>) }
+        { props.expenses.map(expense => (
+            <ExpenseListItem
+                { ...expense }
+                key={expense.id}
+            />
+        )) }
     </div>
 );
 
 const mapStateToProps = (state) => (
     {
-        expenses: state.expenses,
-        filters: state.filters
+        expenses: getVisibleExpenses(state.expenses, state.filters)
     }
 );
 
--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const ExpenseListItem = ({description, amount, createdDate}) => (
+export default ({description, amount, createdDate}) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} at {createdDate}</p>
     </div>
 );
-
-export default ExpenseListItem;
